3ed6f1eb13864f9b69bbfe8d1954551ed766cca9
Jonathan Gruber <gruberjonathan@gmail.com>
2020-05-17 02:42:31 +0200

    Add HTML version of thesis

---
+++ doc/html/generate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+pandoc src/master.tex -f latex -t html -s -o index.html --filter=pandoc-citeproc --bibliography src/references.bib --csl=ieee.csl -M lang:de --toc --toc-depth=3 && prettier --write --print-width=120 index.html
